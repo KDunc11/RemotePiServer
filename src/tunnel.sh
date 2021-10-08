ef6a2399e30b6660c01084fb7db4d56191617606
Kyle Duncan <kduncan@nnu.edu>
2021-10-07 18:33:03 -0700

    Fixed files

--- src/tunnel.sh
+++ src/tunnel.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 createTunnel(){   
-  /usr/bin/ssh -N -R 60000:localhost:22 emailuser@EXTERNALIP
+  /usr/bin/ssh -N -R 60000:localhost:22 emailuser@EXTERNALIP # replace emailuser with the name of an email account that is granted access to the virtual machine
+                                                             # replace EXTERNALIP with the external IP address of the virtual machine, can be obtained from GCP
    
   if [[ $? -eq 0 ]]; then
     echo Tunnel to jumpbox created successfully   
